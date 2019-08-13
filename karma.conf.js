c15089d56ccee90d95729aa13133ad0b95bd5cf7
tpluscode <tpluscode@users.noreply.github.com>
2019-08-13 19:09:26 +0200

    build: update open-wc

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies,@typescript-eslint/no-var-requires */
-const createDefaultConfig = require('@open-wc/testing-karma/default-config')
-const merge = require('webpack-merge')
+const { createDefaultConfig } = require('@open-wc/testing-karma')
+const merge = require('deepmerge')
 
 module.exports = config => {
   config.set(
@@ -11,14 +11,19 @@ module.exports = config => {
         //
         // npm run test -- --grep test/foo/bar.test.js
         // npm run test -- --grep test/bar/*
-        { pattern: config.grep ? config.grep : 'test/**/*.test.js', type: 'module' },
+        { pattern: config.grep ? config.grep : 'test/**/*.test.ts', type: 'module' },
       ],
 
       // you can overwrite/extend the config further
       coverageIstanbulReporter: {
-        reports: ['json'],
         skipFilesWithNoCoverage: true,
         thresholds: null,
+      },
+
+      esm: {
+        babel: true,
+        nodeResolve: true,
+        fileExtensions: ['.ts'],
       },
     }),
   )
